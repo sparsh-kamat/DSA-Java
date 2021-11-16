d098d3f96028aecf866186929efa99c709312738
Sparsh Kamat <90212861+sparsh-kamat@users.noreply.github.com>
2021-11-17 02:25:48 +0530

    bitwise operator codes.

---
+++ Math/src/com/sparsh/Bitwise/PowOfTwo.java
@@ -0,0 +1,10 @@
+package com.sparsh.Bitwise;
+
+public class PowOfTwo {
+    public static void main(String[] args) {
+        int n = 32; // note: fix for n = 0
+        boolean ans = (n & (n-1)) == 0;
+        System.out.println(ans);
+    }
+}
+
